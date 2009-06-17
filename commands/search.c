d7af84c2cc29ef012f1f1a1ce8f8859852960685
Colin Watson <cjwatson@ubuntu.com>
2009-06-17 13:05:47 +0100

    Import upstream version 1.96+20090611

--- commands/search.c
+++ commands/search.c
@@ -1,7 +1,7 @@
 /* search.c - search devices based on a file or a filesystem label */
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2005,2007,2008  Free Software Foundation, Inc.
+ *  Copyright (C) 2005,2007,2008,2009  Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -29,15 +29,16 @@
 
 static const struct grub_arg_option options[] =
   {
-    {"file", 'f', 0, "search devices by a file (default)", 0, 0},
-    {"label", 'l', 0, "search devices by a filesystem label", 0, 0},
-    {"fs-uuid", 'u', 0, "search devices by a filesystem UUID", 0, 0},
-    {"set", 's', GRUB_ARG_OPTION_OPTIONAL, "set a variable to the first device found", "VAR", ARG_TYPE_STRING},
+    {"file",		'f', 0, "search devices by a file (default)", 0, 0},
+    {"label",		'l', 0, "search devices by a filesystem label", 0, 0},
+    {"fs-uuid",		'u', 0, "search devices by a filesystem UUID", 0, 0},
+    {"set",		's', GRUB_ARG_OPTION_OPTIONAL, "set a variable to the first device found", "VAR", ARG_TYPE_STRING},
+    {"no-floppy",	'n', 0, "do not probe any floppy drive", 0, 0},
     {0, 0, 0, 0, 0, 0}
   };
 
 static void
-search_label (const char *key, const char *var)
+search_fs (const char *key, const char *var, int no_floppy, int is_uuid)
 {
   int count = 0;
   auto int iterate_device (const char *name);
@@ -46,21 +47,30 @@ search_label (const char *key, const char *var)
     {
       grub_device_t dev;
       int abort = 0;
-      
+
+      /* Skip floppy drives when requested.  */
+      if (no_floppy &&
+	  name[0] == 'f' && name[1] == 'd' &&
+	  name[2] >= '0' && name[2] <= '9')
+	return 0;
+
       dev = grub_device_open (name);
       if (dev)
 	{
 	  grub_fs_t fs;
-	  
+
 	  fs = grub_fs_probe (dev);
-	  if (fs && fs->label)
+
+#define QUID(x)	(is_uuid ? (x)->uuid : (x)->label)
+
+	  if (fs && QUID(fs))
 	    {
-	      char *label;
-	      
-	      (fs->label) (dev, &label);
-	      if (grub_errno == GRUB_ERR_NONE && label)
+	      char *quid;
+
+	      (QUID(fs)) (dev, &quid);
+	      if (grub_errno == GRUB_ERR_NONE && quid)
 		{
-		  if (grub_strcmp (label, key) == 0)
+		  if (grub_strcmp (quid, key) == 0)
 		    {
 		      /* Found!  */
 		      count++;
@@ -72,80 +82,26 @@ search_label (const char *key, const char *var)
 		      else
 			  grub_printf (" %s", name);
 		    }
-		  
-		  grub_free (label);
+
+		  grub_free (quid);
 		}
 	    }
-	  
+
 	  grub_device_close (dev);
 	}
 
       grub_errno = GRUB_ERR_NONE;
       return abort;
     }
-  
+
   grub_device_iterate (iterate_device);
-  
+
   if (count == 0)
     grub_error (GRUB_ERR_FILE_NOT_FOUND, "no such device: %s", key);
 }
 
 static void
-search_fs_uuid (const char *key, const char *var)
-{
-  int count = 0;
-  auto int iterate_device (const char *name);
-
-  int iterate_device (const char *name)
-    {
-      grub_device_t dev;
-      int abort = 0;
-
-      dev = grub_device_open (name);
-      if (dev)
-	{
-	  grub_fs_t fs;
-	  
-	  fs = grub_fs_probe (dev);
-	  if (fs && fs->uuid)
-	    {
-	      char *uuid;
-	      
-	      (fs->uuid) (dev, &uuid);
-	      if (grub_errno == GRUB_ERR_NONE && uuid)
-		{
-		  if (grub_strcasecmp (uuid, key) == 0)
-		    {
-		      /* Found!  */
-		      count++;
-		      if (var)
-			{
-			  grub_env_set (var, name);
-			  abort = 1;
-			}
-		      else
-			grub_printf (" %s", name);
-		    }
-		  
-		  grub_free (uuid);
-		}
-	    }
-	  
-	  grub_device_close (dev);
-	}
-
-      grub_errno = GRUB_ERR_NONE;
-      return abort;
-    }
-  
-  grub_device_iterate (iterate_device);
-  
-  if (count == 0)
-    grub_error (GRUB_ERR_FILE_NOT_FOUND, "no such device: %s", key);
-}
-
-static void
-search_file (const char *key, const char *var)
+search_file (const char *key, const char *var, int no_floppy)
 {
   int count = 0;
   char *buf = 0;
@@ -157,7 +113,13 @@ search_file (const char *key, const char *var)
       char *p;
       grub_file_t file;
       int abort = 0;
-      
+
+      /* Skip floppy drives when requested.  */
+      if (no_floppy &&
+	  name[0] == 'f' && name[1] == 'd' &&
+	  name[2] >= '0' && name[2] <= '9')
+	return 0;
+
       len = grub_strlen (name) + 2 + grub_strlen (key) + 1;
       p = grub_realloc (buf, len);
       if (! p)
@@ -165,7 +127,7 @@ search_file (const char *key, const char *var)
 
       buf = p;
       grub_sprintf (buf, "(%s)%s", name, key);
-      
+
       file = grub_file_open (buf);
       if (file)
 	{
@@ -181,15 +143,15 @@ search_file (const char *key, const char *var)
 
 	  grub_file_close (file);
 	}
-      
+
       grub_errno = GRUB_ERR_NONE;
       return abort;
     }
-  
+
   grub_device_iterate (iterate_device);
-  
+
   grub_free (buf);
-  
+
   if (grub_errno == GRUB_ERR_NONE && count == 0)
     grub_error (GRUB_ERR_FILE_NOT_FOUND, "no such device");
 }
@@ -199,19 +161,19 @@ grub_cmd_search (grub_extcmd_t cmd, int argc, char **args)
 {
   struct grub_arg_list *state = cmd->state;
   const char *var = 0;
-  
+
   if (argc == 0)
     return grub_error (GRUB_ERR_INVALID_COMMAND, "no argument specified");
 
   if (state[3].set)
     var = state[3].arg ? state[3].arg : "root";
-  
+
   if (state[1].set)
-    search_label (args[0], var);
+    search_fs (args[0], var, state[4].set, 0);
   else if (state[2].set)
-    search_fs_uuid (args[0], var);
+    search_fs (args[0], var, state[4].set, 1);
   else
-    search_file (args[0], var);
+    search_file (args[0], var, state[4].set);
 
   return grub_errno;
 }
--- loader/i386/pc/multiboot2.c
+++ loader/i386/pc/multiboot2.c
@@ -17,8 +17,8 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <multiboot2.h>
 #include <grub/multiboot2.h>
+#include <multiboot2.h>
 #include <grub/elf.h>
 #include <grub/err.h>
 #include <grub/machine/loader.h>
@@ -33,7 +33,7 @@ grub_mb2_arch_elf32_hook (Elf32_Phdr *phdr, UNUSED grub_addr_t *addr)
 
   if ((paddr < grub_os_area_addr)
       || (paddr + phdr->p_memsz > grub_os_area_addr + grub_os_area_size))
-    return grub_error(GRUB_ERR_OUT_OF_RANGE,"Address 0x%x is out of range", 
+    return grub_error(GRUB_ERR_OUT_OF_RANGE,"Address 0x%x is out of range",
                       paddr);
 
   return GRUB_ERR_NONE;
@@ -82,7 +82,7 @@ void
 grub_mb2_arch_unload (struct multiboot_tag_header *tags)
 {
    struct multiboot_tag_header *tag;
-   
+
    /* Free all module memory in the tag list.  */
    for_each_tag (tag, tags)
      {
